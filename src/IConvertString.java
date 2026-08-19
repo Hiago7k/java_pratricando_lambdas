3c0805c70670f0bcbd8005c08747ad421f378ad4
Hiago 7k <hiago7k@gmail.com>
2026-08-19 19:36:17 -0300

    converssor string

---
+++ src/IConvertString.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface IConvertString {
+    String converter(String texto);
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import static java.lang.Character.toUpperCase;
+
 class Main{
     static void main() {
 
         iMultiplicacao mult = (a,b) -> a * b;
         System.out.println(mult.executar(3,5));
+
+        //3 - Crie uma função lambda que receba uma string
+        // e a converta para letras maiúsculas.
+        IConvertString maisculo = (texto) -> texto.toUpperCase();
+        System.out.println(maisculo.converter("teste"));
     }
 }

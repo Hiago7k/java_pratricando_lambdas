7274ded82fccb344ad4ef740034754f565ea2e8b
Hiago 7k <hiago7k@gmail.com>
2026-08-20 16:59:29 -0300

    fixing

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 import static java.lang.Character.reverseBytes;
 import static java.lang.Character.toUpperCase;
+import static java.util.Collections.list;
 import static java.util.Collections.reverse;
 
 class Main{
     static void main() {
+        INumeros numeros = lista -> lista.add(1);
+        numeros.replaceAll(n * 3);
 
 
 
-        StringBuilder testando = new StringBuilder();
-        IPalidromo converte = str -> str.equals(new StringBuilder(str).reverse().toString());
-        System.out.println(converte.reverso("radar"));
-        System.out.println(converte.reverso("java"));
     }
 }
